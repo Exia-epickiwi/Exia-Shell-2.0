9c4f5c2c7fe38db13248f451b124e601a34d71c1
Baptiste Saclier <baptiste.saclier@viacesi.fr>
2015-12-10 22:17:25 +0100

    Code de base

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Fonction init du shell
+//Parametres :
+//  argc : Nombre d'arguments passés
+//  argv : Tableau d'arguments passés
+//Renvoie : Code de sortie du programme
+int main(int argc, char *argv[]){
+  //Chargement de la configuration
+  //config *configuration = loadConfig(); TODO Non implémenté
+  /*switch(config->mode){
+    case 0:
+      //Mode expert
+      //return initExpertMode();  TODO Non implémenté
+    break;
+    case 1:
+      //Mode pas a pas
+      //return initAssistantMode(); TODO Non implémenté
+    break;
+    case 2:
+      //Mode naturel
+      //return initNaturalMode(); TODO Non implémenté
+    break;
+    default:
+      printf("Erreur! le mode %d n'est pas un mode connu",config-Wmode);
+      printf("Les modes disponibles sont :\n0 : Mode expert\n1 : Mode assistant\n2 : Mode naturel");
+      return EXIT_FAILURE;
+    break;
+  }*/
+}
