32248232a5573553acfbeac47e2a0cbd9d643263
EpicKiwi <lelycandu76@gmail.com>
2015-12-11 11:27:37 +0100

    Ajout du prototype

--- language.h
+++ language.h
@@ -20,5 +20,6 @@ typedef struct Language{
 Language* loadLanguage(char *file);
 Language* parseLanguageFile(FILE *langFile);
 void addLanguageString(Language *target,char *key, char *value);
+char* toLocaleString(Language *lang,char *key);
 
 #endif
