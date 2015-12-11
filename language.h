de5ecb920e25e5589f8057fbebf5b7b8b3607a56
EpicKiwi <lelycandu76@gmail.com>
2015-12-11 09:54:49 +0100

    Correction des structures

--- language.h
+++ language.h
@@ -1,15 +1,19 @@
 #ifndef _LANGUAGEHEADER_
 #define _LANGUAGEHEADER_
 
-typedef struct LangString{
+typedef struct LangString LangString;
+
+struct LangString{
   char key[KEY_LENGTH];
   char value[COMMAND_LENGTH];
   LangString *next;
-} LangString;
+};
 
 typedef struct Language{
   LangString *first;
   int length;
 } Language;
+
+//Language* loadLanguage(char *file);
 
 #endif
