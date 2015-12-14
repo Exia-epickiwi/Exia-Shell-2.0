e560f0e35079882c506a5fb7102a1fa52b82eb31
Nicos <nicolas.mazard@viacesi.fr>
2015-12-14 11:08:19 +0100

    Fix Bug command ExpertMode

--- expert.c
+++ expert.c
@@ -8,17 +8,17 @@
 #include "log.h"
 
 int initExpertMode(Config *configuration) {
-  char commande[256];
+  char commande[250];
   char pathCommande[] = "/bin/";
   do {
-
+    char cmd[256];
     printPrompt(configuration->prompt);
     if(getKeyboard(commande, 256) == 0) return 0;
 
     if(strcmp(commande, "exit") == 0) break;
-    if((commande[0] != '.' && commande[1] != '/') || commande[0] != '/') strcat(pathCommande, commande);
-    execCommandSync(pathCommande);
 
+    if((commande[0] != '.' && commande[1] != '/') || commande[0] != '/') sprintf(cmd, "%s%s", pathCommande, commande);
+    execCommandSync(cmd);
     toLog(commande);
   }while(1);
  return 0;
