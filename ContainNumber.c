ae69e6fada4d6247c45ee239116ddba5564315d7
FranzouilleWazaah <franz.alliod@viacesi.fr>
2015-12-14 16:09:48 +0100

    Create ContainNumber.c

---
+++ ContainNumber.c
@@ -0,0 +1,21 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    int main(void)
+    {
+      
+        int ContainNumber (char**words);
+        char *pend = NULL;
+        while( i<1; next ;i++) {
+        do
+        {
+            printf("Tappez un numerateur: ");
+            fflush(stdout);
+            fgets(tampon, sizeof tampon, stdin);
+            fclean(tampon, stdin);
+            nombre = strtol(tampon, &pend, 0);
+        }
+        while (*pend != 0);
+        return NULL;
+        return words [I];
+    }
+  }
