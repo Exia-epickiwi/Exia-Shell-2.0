ff6dd2f75c0907bc155957863d0f691d0cce1374
FranzouilleWazaah <franz.alliod@viacesi.fr>
2015-12-14 15:38:05 +0100

    Create ContainNumberPrototype.c

---
+++ ContainNumberPrototype.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib>
+int ContainNumber (char**words)
+while( i<1; next ;i++) {
+cout << "nombre:";
+while( !(cin >> ContainNumber) ) {
+    string tmp;
+    cin.clear();
+    cin >> tmp;
+    cout << "incorrect" << endl;
+return NULL;
+return words [I];
+}
+}
