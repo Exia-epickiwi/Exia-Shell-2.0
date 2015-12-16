21e83ad9012742bb870beede5fed728ddc742d62
FranzouilleWazaah <franz.alliod@viacesi.fr>
2015-12-16 15:15:15 +0100

    Create Wmount.c

---
+++ Wmount.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/mount.h>
+
+int main()
+{
+   const char* src  = "none";
+   const char* target = "/var/tmp"; /* /var/tmp = stockage de fichiers temporaires */
+   const char* type = "tmpfs"; /* tmpfs est le nom donné au système de fichier Unix temporaire */
+   const unsigned long mntflags = 0;
+   const char* opts = "mode=0700,uid=65534";   /* 65534 est l'UID de personne, 0700 = mode répertoire */
+
+   int result = mount(src, target, type, mntflags, opts);
+
+   if (result == 0)
+   {
+      printf("Montage créer dans %s...\n", target);
+      printf("Appuyer sur <return> pour démonter le volume: ");
+      getchar();
+
+      umount(target);
+   }
+   else
+   {
+      printf("Erreur : montage impossible %s\n"
+             "Raison: %s [%d]\n",
+             src, strerror(errno), errno);
+      return -1;
+   }
+
+   return 0;
+}
