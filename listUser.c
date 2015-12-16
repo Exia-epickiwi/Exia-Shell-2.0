635635df74f27b1f059ad221489786f712ea11f2
The-Nico26 <nicolas.mazard@viacesi.fr>
2015-12-16 23:04:42 +0100

    Rajout des commentaires + Vérification de la gestion de la mémoire

--- listUser.c
+++ listUser.c
@@ -9,19 +9,26 @@
 #include "color.h"
 
 void listUser(Config *config){
+  //ouvre le fichier
   FILE *fileUser = fopen("/etc/passwd", "r");
   if(fileUser == NULL){
+    //Si le fichier n'existe pas alors aucun utilisateur
     printf("%s\n", toLocaleString(config->lang, "error.userNull"));
     return;
   }
   char user[255];
-  printf(COLOR_GREEN"UID\t|  "COLOR_YELLOW" USER\n-------------------\n"COLOR_RESET);
+  printf("\t"COLOR_GREEN"UID\t|  "COLOR_YELLOW" USER\n\t"COLOR_MAGENTA"-----------------\n"COLOR_RESET);
+  //affichage de chaque titre pour les colonnes
   while(fgets(user, 255, fileUser)){
+    //sépare et récupére la valeur de name
     char *name = strtok(user, ":");
+    //La second valeur est x donc on la garde pas
     strtok(NULL, ":");
+    //La troisième valeur est l'uid de l'utilisateur donc on la garde.
     char *uid = strtok(NULL, ":");
-    if(atoi(uid) >= 1000 || atoi(uid) == 0) {
-      printf(COLOR_GREEN"%s\t|  "COLOR_YELLOW" %s\n"COLOR_RESET, uid, name);
+    //On rajoute tout PID de 1000 sans le nobody et on rajoute le root au PID = 0
+    if((atoi(uid) >= 1000 || atoi(uid) == 0) && strcmp(name, "nobody") != 0) {
+      printf("\t"COLOR_GREEN"%s\t|  "COLOR_YELLOW" %s\n"COLOR_RESET, uid, name);
     }
   }
 }
--- motd.c
+++ motd.c
@@ -4,12 +4,19 @@
 #include "motd.h"
 
 void printStartsInfos(char *locale){
+  //récupére la constante du PATH_MOTD
   char path[] = PATH_MOTD;
+  //on addition les 2 chaines de caractéres
   char *nameFile = strcat(path, locale);
+  //Ouvre le fichier
   FILE *file = fopen(nameFile, "r");
   char line[256];
+  //parcour tout les lignes du fichier
   while(fgets(line, 256, file)){
+    //affiches les lignes
     printf("%s", line);
   }
   printf("\n");
+  //ferme le fichier
+  fclose(file);
 }
