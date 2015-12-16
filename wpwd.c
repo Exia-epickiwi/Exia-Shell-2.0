a3e6606602f5f76e4ba714dce61d7ec5314d7fd7
The-Nico26 <nicolas.mazard@viacesi.fr>
2015-12-16 14:02:29 +0100

    Rajout des fichiers

--- wpwd.c
+++ wpwd.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "wpwd.h"
+#include "color.h"
 
 void getPwd() {
     char cwd[1025];
     getcwd(cwd, sizeof(cwd));
-    printf("%s\n", cwd);
+    printf(COLOR_GREEN "%s\n" COLOR_RESET, cwd);
 }
