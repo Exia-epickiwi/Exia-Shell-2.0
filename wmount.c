87efc9b67cd1aa316c3a6296476245f95255cf98
FranzouilleWazaah <franz.alliod@viacesi.fr>
2015-12-16 15:21:46 +0100

    Update wmount.c

--- wmount.c
+++ wmount.c
@@ -8,18 +8,14 @@ int main()
    const char* src  = "none";
    const char* target = "/var/tmp"; /* /var/tmp = stockage de fichiers temporaires */
    const char* type = "tmpfs"; /* tmpfs est le nom donné au système de fichier Unix temporaire */
-   const unsigned long mntflags = 0;
+   const unsigned long mntflags = 1;
    const char* opts = "mode=0700,uid=65534";   /* 65534 est l'UID de personne, 0700 = mode répertoire */
 
    int result = mount(src, target, type, mntflags, opts);
 
    if (result == 0)
    {
-      printf("Montage créer dans %s...\n", target);
-      printf("Appuyer sur <return> pour démonter le volume: ");
-      getchar();
-
-      umount(target);
+      return 1
    }
    else
    {
