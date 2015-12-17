d3578646e1764420fb86fbd0200feedb8fea9f81
FranzouilleWazaah <franz.alliod@viacesi.fr>
2015-12-17 10:59:28 +0100

    Correction orthographe com

--- config.c
+++ config.c
@@ -12,7 +12,7 @@ void moveIndex(char config[], int start, int end);
 
 //Fonction permettant de charger des variables à partir d'un dossier
 Config *loadConfig(){
-  //initialise et ouvre le fichier en lecture seul
+  //Initialise et ouvre le fichier en lecture seule
   FILE *fileConfig;
   fileConfig = fopen(PATH_PROFILE, "r");
   //Initialise la structure
@@ -37,12 +37,12 @@ Config *loadConfig(){
       if(strcmp(keyword, "mode") == 0){
         config->mode = atoi(lineConfig);
       }
-      //si c'est égale à language
+      //Si c'est égale à language
       else if(strcmp(keyword, "language") == 0){
         //Copie le reste de la lineConfig dans la config de locale
         strcpy(config->locale, lineConfig);
       }
-      //si c'est égale à prompt
+      //Si c'est égale à prompt
       else if(strcmp(keyword, "prompt") == 0){
         //Copie le reste de la lineConfig dans la config de prompt
         strcpy(config->prompt, lineConfig);
@@ -56,14 +56,14 @@ Config *loadConfig(){
   loadHistory(config);
   //Ferme le fichier correctement
   fclose(fileConfig);
-  //retourne la structure config initialisée
+  //Retourne la structure config initialisée
   return config;
 }
-//fonction pour séparer une chaine de caractère à partir d'un char de séparation
+//Fonction pour séparer une chaine de caractère à partir d'un char de séparation
 char *separationChain(char config[], char separation){
   //initialise les variables des boucles for => Pour certain c cela ne marche pas les déclarations dans les boucles for
   int a, b;
-  //boucle for qui parcours le tableau config[]
+  //Boucle for qui parcourt le tableau config[]
   for (a = 0; a < LINE_CHARACTER_MAX && (config[a] != '\0' || config[a] != '\n'); a++) {
     //Si le caractère est égale au caractère
     if(config[a] == separation){
@@ -76,20 +76,20 @@ char *separationChain(char config[], char separation){
       s[b+1] = '\0';
       //Déplace le tableau en enlevant le tableau s[]
       moveIndex(config, a+1, LINE_CHARACTER_MAX);
-      //renvoie le tableau s
+      //Renvoie le tableau s
       return s;
     }
   }
   return NULL;
 }
-//Fonction permettant de déplacer une chaine de caractère dans un tableau
+//Fonction permettant de déplacer une chaîne de caractère dans un tableau
 void moveIndex(char config[], int start, int end){
   int a;
-  //Boucle for qui parcours le tableau config[]
+  //Boucle for qui parcourt le tableau config[]
   for (a = 0; a < end && config[a] != '\0'; a++) {
     //Remplace l'index de config[a] par l'index start+a
     config[a] = config[start+a];
-    //si le caractère est égale à \n et \r alors le remplace par \0
+    //Si le caractère est égale à \n et \r alors le remplace par \0
     if(config[a] == '\n') {
       config[a] = '\0';
     } else if(config[a] == '\r') {
